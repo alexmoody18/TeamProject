8717b240a12853831d9113ed8467e61356a75f5e
GATESTC22 <gatestc22@gcc.edu>
2023-02-27 10:43:21 -0500

    i did stuff

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hi");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name?");
+        System.out.println("Hello " + sc.next());
     }
 }
